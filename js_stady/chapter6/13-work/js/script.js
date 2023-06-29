9c7923164e53a421204df2fc9ee72aac888cfab8
kuro-siba <siba-kuro@nike.eonet.ne.jp>
2023-06-30 08:17:36 +0900

    20230630-08

--- js_stady/chapter6/13-work/js/script.js
+++ js_stady/chapter6/13-work/js/script.js
@@ -1,7 +1,11 @@
 
 
 const showkirin = (entries) => {
-  console.log(entries[0].target);
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['200px 0', 0], 
+  };
+  entries[0].target.animate(keyframes, 600);
 };
 
 const kirinObserver = new IntersectionObserver(showkirin);
