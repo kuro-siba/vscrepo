181514baff2de3ff8933f5d916d0e4fc0435d8d4
kuro-siba <siba-kuro@nike.eonet.ne.jp>
2023-06-30 23:01:30 +0900

    20230630-23

--- js_stady/chapter3/ColorPicker work/js/script.js
+++ js_stady/chapter3/ColorPicker work/js/script.js
@@ -1,1 +1,8 @@
+// console.log('準備完了！');
+// console.log(document.querySelector('#colorPicker'));
+// console.log(document.querySelector('#colorPicker').value);
+// document.querySelector('#colortext').textContent = `カラーコード：${document.querySelector('#colorPicker').value}`;
+const text = document.querySelector('#colortext');
+const color = document.querySelector('#colorPicker');
 
+text.textContent = `カラーコード：${color.value}`;
