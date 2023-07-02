63b27544ea909576bf14247d19cbeb0b77e7492b
kuro-siba <siba-kuro@nike.eonet.ne.jp>
2023-07-02 10:41:07 +0900

    20230702-11

--- js_stady/chapter3/ColorPicker work/js/script.js
+++ js_stady/chapter3/ColorPicker work/js/script.js
@@ -1,8 +1,27 @@
-// console.log('準備完了！');
-// console.log(document.querySelector('#colorPicker'));
-// console.log(document.querySelector('#colorPicker').value);
-// document.querySelector('#colortext').textContent = `カラーコード：${document.querySelector('#colorPicker').value}`;
+
+// document.querySelector('')は、HTML全体を表すdocumentオブジェクトの名からセレクターを持つ"要素"を取得
+  // ここでは、htmlから[<p id="colortext">カラーコードを検索</p>]の要素が取得できる
 const text = document.querySelector('#colortext');
+  // ここでは、htmlから[<input id="colorPicker" type="color">]の要素が取得できる
 const color = document.querySelector('#colorPicker');
 
+// id="colortext"のp要素のテキストを変える
+//  補足：JavaScriptを使うことで表示されているWebページを後から変更することができる
+//  .textContentで、要素内のテキストを変えることができる
+  // 何に変えるかは、
+  //    先頭に、カラーコードの文字列
+  //    その後に、id="colorPicker"のinput要素の値（#000000など）
 text.textContent = `カラーコード：${color.value}`;
+
+
+
+
+
+
+// カラーピッカーで色を選んだときにイベントを動作させる
+  // 何が=>色が；最初のcolorは、定義された定数：colorでinput要素のこと・・・これがきっかけとなる要素
+  // addEventListenerはイベントを設定する時のメソッド
+  // カッコの中に
+  //  1.イベント名：input=入力されたときにイベントが発生する
+  //  2.イベントが起きた時の処理を設定　ここでは、colorBgという処理(関数)
+color.addEventListener('input',colorBg);
